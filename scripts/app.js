c9b4f3b510cdca266824c1f698b7185b2ce1d58c
CptSurfinBird <jacobgittings@gmail.com>
2016-02-11 10:37:49 -0500

    Updated the firstParagraph.innerHTML in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,18 +6,18 @@
 	console.log("App Started...");
 	
 	// declared a named function <- use for Tom's class
-	/*
 	function replaceFirstParagraph() {
 		console.log("inside replaceFirstParagraph function");
 		var firstParagraph;
 		
 		firstParagraph = document.getElementById("firstParagraph");
 		
-		firstParagraph.innerHTML = "My New Paragraph Data";
+		firstParagraph.innerHTML = "I am a web developer from Barrie, Ontario. My goals are to create new and inavative web soloutions for the future.";
 	}
-	*/
+
 	
 	// declare an anonymous function with named alias
+	/*
 	var replaceFirstParagraph = function() {
 		console.log("inside replaceFirstParagraph function");
 		var firstParagraph;
@@ -26,7 +26,7 @@
 		
 		firstParagraph.innerHTML = "My New Paragraph Data";		
 	};
-	
+	*/
 	
 	// call the replaceFirstParagraph function
 	replaceFirstParagraph();
