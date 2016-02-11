b739a7ebebb652cd37193309decfa9ec2a1b778b
CptSurfinBird <jacobgittings@gmail.com>
2016-02-11 15:29:40 -0500

    made some more changes to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,18 @@
 
 // setup your IIFE (Immediatley Invoked Function Expression)
 (function(){
-	
+
+	var paragraphData = ["First Paragraph", "Second Paragraph", "Third Paragraph", "Fourth Paragraph"];
+
 	console.log("App Started...");
-	
+
+	var paragraphDataLength = ParagraphData.Length;
+	for(var index= 0; index < paragraphDataLength; index++ ) {
+		console.log(paragraphData[index]);
+	}
+
+
+
 	// declared a named function <- use for Tom's class
 	function replaceFirstParagraph() {
 		console.log("inside replaceFirstParagraph function");
@@ -13,15 +22,6 @@
 		firstParagraph = document.getElementById("firstParagraph");
 		
 		firstParagraph.innerHTML = "I am a web developer from Barrie, Ontario. My goals are to create new and inavative web soloutions for the future.";
-	}
-
-		function replaceSecondParagraph() {
-		console.log("inside replaceSecondParagraph function");
-		var secondParagraph;
-		
-		secondParagraph = document.getElementById("secondParagraph");
-		
-		secondParagraph.innerHTML = "This is update on an update, whoa!";
 	}
 
 	// declare an anonymous function with named alias
