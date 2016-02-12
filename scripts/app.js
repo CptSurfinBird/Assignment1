d748f4574b5618df2ff19100e9f631b550b5f592
CptSurfinBird <jacobgittings@gmail.com>
2016-02-12 14:09:41 -0500

    added footers

--- scripts/app.js
+++ scripts/app.js
@@ -4,27 +4,27 @@
 	console.log("App started");
 
 	var introduction;
-	introduction = document.getElementById("introduction");
-	introduction.innerHTML = "My name is Jacob Gittings. I am a Web Developer from Barrie, Ontaio. My goal is to develop, provide, and maintain web solutions for the future.";
+		introduction = document.getElementById("introduction");
+		introduction.innerHTML = "My name is Jacob Gittings. I am a Web Developer from Barrie, Ontaio. My goal is to develop, provide, and maintain web solutions for the future.";
 
 	var about_me;
-	about_me = document.getElementById("about_me");
-	about_me.innerHTML = "I am currently an IWDD (Interactive Web Development & Design) student at Georgian College. My goal is to provide well crafted and well supported web solutions.";
+		about_me = document.getElementById("about_me");
+		about_me.innerHTML = "I am currently an IWDD (Interactive Web Development & Design) student at Georgian College. My goal is to provide well crafted and well supported web solutions.";
 
 	var my_project1;
-	my_project1 = document.getElementById("my_project1");
-	my_project1.innerHTML = "The object of this project was to create a multi-page site with a functioning nav bar. I also made it as responsive as I was able to at the time.";
+		my_project1 = document.getElementById("my_project1");
+		my_project1.innerHTML = "The object of this project was to create a multi-page site with a functioning nav bar. I also made it as responsive as I was able to at the time.";
 
 	var my_project2;
-	my_project2 = document.getElementById("my_project2");
-	my_project2.innerHTML = "Cover UP!";
+		my_project2 = document.getElementById("my_project2");
+		my_project2.innerHTML = "Cover UP!";
 
 	var my_project3;
-	my_project3 = document.getElementById("my_project3");
-	my_project3.innerHTML = "COVER UP!!";
+		my_project3 = document.getElementById("my_project3");
+		my_project3.innerHTML = "COVER UP!!";
 
 	var contact_me;
-	contact_me = document.getElementById("contact_me");
-	contact_me.innerHTML = "Feel free to send me a message";
+		contact_me = document.getElementById("contact_me");
+		contact_me.innerHTML = "Feel free to send me a message";
 
 })();
