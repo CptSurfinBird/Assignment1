5b9a12df5385a2a40369b7fe9ce24034144d7a5e
CptSurfinBird <jacobgittings@gmail.com>
2016-02-12 13:23:25 -0500

    Trying to fix app.js problem

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,30 @@
 "use strict";
 
-(function() {
+(function () {
 	console.log("App started");
 
 	var introduction;
-		introduction = document.getElementById("introduction");
-		introduction.innerHTML = "My name is Jacob Gittings. I am a Web Developer from Barrie, Ontaio. My goal is to develop, provide, and maintain web solutions for the future.";
+	introduction = document.getElementById("introduction");
+	introduction.innerHTML = "My name is Jacob Gittings. I am a Web Developer from Barrie, Ontaio. My goal is to develop, provide, and maintain web solutions for the future.";
 
 	var about_me;
-		about_me = document.getElementById("about_me");
-		about_me.innerHTML = "I am currently an IWDD (Interactive Web Development & Design) student at Georgian College. My goal is to provide well crafted and well supported web solutions.";
+	about_me = document.getElementById("about_me");
+	about_me.innerHTML = "I am currently an IWDD (Interactive Web Development & Design) student at Georgian College. My goal is to provide well crafted and well supported web solutions.";
 
 	var my_project1;
-		my_project1 = document.getElementById("my_project1");
-		my_project1.innerHTML = "";
+	my_project1 = document.getElementById("my_project1");
+	my_project1.innerHTML = "COver up";
 
 	var my_project2;
-		my_project2 = document.getElementById("my_project2");
-		my_project2.innerHTML = "";
+	my_project2 = document.getElementById("my_project2");
+	my_project2.innerHTML = "Cover UP!";
 
 	var my_project3;
-		my_project3 = document.getElementById("my_project3");
-		my_project3.innerHTML = "";
+	my_project3 = document.getElementById("my_project3");
+	my_project3.innerHTML = "COVER UP!!";
 
 	var contact_me;
-		contact_me = document.getElementById("contact_me");
-		contact_me.innerHTML = "Feel free to send me a message";
+	contact_me = document.getElementById("contact_me");
+	contact_me.innerHTML = "Feel free to send me a message";
 
 })();
