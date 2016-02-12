047a9634e8e90a859d1377e073d9a2304267fb45
CptSurfinBird <jacobgittings@gmail.com>
2016-02-12 08:41:39 -0500

    updated index.html, app.css, and re-did my app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,14 @@
 "use strict";
 
-// setup your IIFE (Immediatley Invoked Function Expression)
-(function(){
-
-	var paragraphData = ["First Paragraph", "Second Paragraph", "Third Paragraph", "Fourth Paragraph"];
-
-	console.log("App Started...");
-
-	var paragraphDataLength = ParagraphData.Length;
-	for(var index= 0; index < paragraphDataLength; index++ ) {
-		console.log(paragraphData[index]);
-	}
-
-
-
-	// declared a named function <- use for Tom's class
-	function replaceFirstParagraph() {
-		console.log("inside replaceFirstParagraph function");
-		var firstParagraph;
-		
-		firstParagraph = document.getElementById("firstParagraph");
-		
-		firstParagraph.innerHTML = "I am a web developer from Barrie, Ontario. My goals are to create new and inavative web soloutions for the future.";
-	}
-
-	// declare an anonymous function with named alias
-	/*
-	var replaceFirstParagraph = function() {
-		console.log("inside replaceFirstParagraph function");
-		var firstParagraph;
-		
-		firstParagraph = document.getElementById("firstParagraph");
-		
-		firstParagraph.innerHTML = "My New Paragraph Data";		
-	};
-	*/
+(function() {
+	console.log("App started");
 	
-	// call the replaceFirstParagraph function
-	replaceFirstParagraph();
-	
+	var introduction;
+		introduction = document.getElementById("introduction");
+		introduction.innerHTML = "My name is Jacob Gittings. I am a Web Developer.";
+
+	var intro2;
+		intro2 = document.getElementById("intro2");
+		intro2.innerHTML = "SHOW ME POTATO SALAD!";
+
 })();
