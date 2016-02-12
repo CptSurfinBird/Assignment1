bcc304dfdf7dc43aeafc2568edfe6f6b620cad99
CptSurfinBird <jacobgittings@gmail.com>
2016-02-12 12:57:19 -0500

    added new pahes, changing from sigle page design to multi page design

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,29 @@
 
 (function() {
 	console.log("App started");
-	
+
 	var introduction;
 		introduction = document.getElementById("introduction");
-		introduction.innerHTML = "My name is Jacob Gittings. I am a Web Developer.";
+		introduction.innerHTML = "My name is Jacob Gittings. I am a Web Developer from Barrie, Ontaio. My goal is to develop, provide, and maintain web solutions for the future.";
 
-	var intro2;
-		intro2 = document.getElementById("intro2");
-		intro2.innerHTML = "SHOW ME POTATO SALAD!";
+	var about_me;
+		about_me = document.getElementById("about_me");
+		about_me.innerHTML = "I am currently an IWDD (Interactive Web Development & Design) student at Georgian College. My goal is to provide well crafted and well supported web solutions.";
+
+	var my_project1;
+		my_project1 = document.getElementById("my_project1");
+		my_project1.innerHTML = "";
+
+	var my_project2;
+		my_project2 = document.getElementById("my_project2");
+		my_project2.innerHTML = "";
+
+	var my_project3;
+		my_project3 = document.getElementById("my_project3");
+		my_project3.innerHTML = "";
+
+	var contact_me;
+		contact_me = document.getElementById("contact_me");
+		contact_me.innerHTML = "Feel free to send me a message";
 
 })();
